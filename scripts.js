1b5dc3312955ce4e77e593ea22e5fdfc20b7b3a5
kalgcny09 <kalgcny@gmail.com>
2017-05-29 20:58:25 -0400

    updates

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
+
 	var cards = [
+	'<img src="images/monsters-01.png">',
+	'<img src="images/monsters-02.png">',
+	'<img src="images/monsters-03.png">',
+	'<img src="images/monsters-04.png">',
+	'<img src="images/monsters-17.png">',
+	'<img src="images/monsters-18.png">',
 	'<img src="images/monsters-01.png">',
 	'<img src="images/monsters-02.png">',
 	'<img src="images/monsters-03.png">',
@@ -7,67 +14,92 @@ $(document).ready(function(){
 	'<img src="images/monsters-17.png">',
 	'<img src="images/monsters-18.png">'
 	];
+	
+	var copyCards = cards.slice();
+
+	var randomCards = [];
+	shuffleCards();
+
+	var winMessage = '';
 
 	var gridSize = 12;
 	var memoryGameHTML = '';
-	for (let i=0; i< gridSize; i++) {
-		if(i<2) {
-			card = cards[0];
-		}else if(i<4) {
-			card = cards[1];
-		}else if(i<6) {
-			card = cards[2];
-		}else if(i<8){
-			card = cards[3];
-		}else if(i<10){
-			card = cards[4];
-		}else {
-			card = cards[5];
-		}
+	var failedMatches = 0;
+	
 
-		memoryGameHTML +='<div class="card col-sm-3">';
+	for (let i = 0; i < gridSize; i++){
+		var card = randomCards[i];
+
+
+		memoryGameHTML += '<div class="card col-sm-3">';
 			memoryGameHTML += '<div class="card-holder">';
 				memoryGameHTML += `<div class="card-front">${card}</div>`;
 				memoryGameHTML += `<div class="card-back"></div>`;
 			memoryGameHTML += '</div>';
 		memoryGameHTML += '</div>';
-
 	}
+
 	$('.mg-contents').html(memoryGameHTML);
 
-	$('.card-holder').click(function(){
-		$(this).toggleClass('flip'); 
+	$('.peek').click(function(){
+		$('.card-holder').toggleClass('flip');
+		setTimeout(function(){
+			$('.card-holder').removeClass('flip');
+		}, 1000);
+	});
 
-		var cardsUp = $('.flip'); //tags and classes will always return an array
-		if(cardsUp.length == 2) {
+	$('.card-holder').click(function(){
+		$(this).toggleClass('flip');
+
+		
+		var cardsUp = $('.flip');
+		if(cardsUp.length >= 2){
+			
 			var card1 = cardsUp[0].children[0].children[0].src;
 			var card2 = cardsUp[1].children[0].children[0].src;
-			var score = 0;
-			if(card1 == card2) {
+		
+			if (card1 == card2){
+				
 				cardsUp.removeClass('flip');
-				cardsUp.addClass('matched');
+				cardsUp.addClass('matched'); 
 				var matchedCards = $('.matched');
 
 				$('#Score').html("Matches " + matchedCards.length/2);
-				if(matchedCards.length == gridSize){
-					setTimeout(function(){
-						alert("You have won the game!");
-					},1000);
+				if (matchedCards.length == gridSize){
+ 					setTimeout(function(){
+						alert("We beat Team Rocket! Thanks for your help!");
+ 					},1000);
+ 				}
 					
-				}
-			}else {
+			}else{
 				setTimeout(function(){
 					cardsUp.removeClass('flip');
-				},2000);
+				}, 2000);
 			
 			}
+		}	
+	});
+
+	function shuffleCards(){
+		for(let i = 0; i < cards.length; i++){
+			var rand = Math.floor(Math.random() * copyCards.length);
+			randomCards.push(copyCards.splice(rand,1)[0]);
 		}
+	}
 
-
-	})
-
-
+	$('.reset').click(function(){
+		copyCards = cards.slice();
+		randomCards = [];
+		shuffleCards();
+		
+		$('.card-holder').removeClass('flip matched');
+	});
 
 });
 
+
+// if (cardsUp.removeClass('flip')){
+// 		failedMatches += 1;
+// 		$('#teamRocket').html("TEAM ROCKET " + (failedMatches));
+// }
 
